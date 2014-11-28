adc160c97ea945194274099209df0ac1341898d3
Adam Elimelech <adam.elimelech@gmail.com>
2014-11-28 12:38:09 +0000

    Add unimplemented methods

--- PriorityQWorkshop/src/priorityQueue/NaiveLockSprayListPriorityQueue.java
+++ PriorityQWorkshop/src/priorityQueue/NaiveLockSprayListPriorityQueue.java
@@ -2,4 +2,46 @@ package priorityQueue;
 
 public class NaiveLockSprayListPriorityQueue extends SprayListPriorityQueue {
 
+	@Override
+	protected void startInsert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void endInsert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected int randomLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void startDeleteMin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void endDeleteMin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected int getNumberOfThreads() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int randomStep(int max) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
